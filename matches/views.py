295cac259836353d34f4138275b8e14b024ac384
Sergio <sergio.pm.developer@gmail.com>
2024-04-28 01:12:44 +0000

    feat: implement initial version of results page and its resources

--- matches/views.py
+++ matches/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def results(request: HttpRequest) -> HttpResponse:
+    """
+    Render the results page.
+
+    Parameters
+    ----------
+    request : HttpRequest
+        The request object.
+
+    Returns
+    -------
+    HttpResponse
+        The results page.
+    """
+
+    return render(request, "matches/results.html")
