ce1c48e3eab4a073d25209f12b34a2c79e852189
Sergio <sergio.pm.developer@gmail.com>
2024-05-21 23:24:03 +0000

    refactor:test: implement pipeline generic classes and modify command and unit tests

--- ai/interfaces.py
+++ ai/interfaces.py
@@ -1,89 +1,192 @@
-from abc import ABC, abstractmethod
+import json
+
+import joblib
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import Pipeline
+
+from ai.preprocessing import NumericalScaler
 
 
-class AbstractPipeline(ABC):
+class PipelineGeneric:
     """
-    Abstract class for pipeline
+    PipelineGeneric class
 
     Methods
     -------
     _initialize_pipeline_steps() -> None
-        Abstract method to initialize the pipeline steps
+        Initialize the pipeline steps
+    _add_steps_to_pipeline(steps: list[str]) -> None
+        Add steps to pipeline
     _initialize_pipeline() -> None
-        Abstract method to initialize the pipeline
+        Initialize the pipeline
     _execute_pipeline() -> None
-        Abstract method to execute the pipeline
-    _export_pipeline() -> None
-        Abstract method to export the pipeline
+        Execute the pipeline
+    _export_pipeline(pipeline_dir: str) -> None
+        Export the pipeline
     """
 
-    @abstractmethod
     def _initialize_pipeline_steps(self) -> None:
         """
-        Abstract method to initialize the pipeline steps
+        Initialize the pipeline steps
         """
 
-        pass
+        self._pipeline_steps = []
 
-    @abstractmethod
+    def _add_steps_to_pipeline(self, steps: list[str]) -> None:
+        """
+        Add steps to pipeline
+
+        Parameters
+        ----------
+        steps : list[str]
+            List of steps to add to the pipeline
+        """
+
+        AVAILABLE_STEPS = {
+            "numerical_scaler": ("scale_features", NumericalScaler()),
+        }
+
+        for step in steps:
+            try:
+                self._pipeline_steps.append(AVAILABLE_STEPS[step])
+            except KeyError:
+                raise KeyError(f"Step {step} not available")
+
     def _initialize_pipeline(self) -> None:
         """
-        Abstract method to initialize the pipeline
+        Initialize the pipeline
         """
 
-        pass
+        self._pipeline = Pipeline(self._pipeline_steps)
 
-    @abstractmethod
     def _execute_pipeline(self) -> None:
         """
-        Abstract method to execute the pipeline
+        Execute the pipeline
         """
 
-        pass
+        try:
+            self._pipeline.fit_transform(self.X)
+        except AttributeError:
+            raise AttributeError("X is not defined")
 
-    @abstractmethod
-    def _export_pipeline(self) -> None:
+    def _export_pipeline(self, pipeline_dir: str) -> None:
         """
-        Abstract method to export the pipeline
+        Export the pipeline
+
+        Parameters
+        ----------
+        pipeline_dir : str
+            Pipeline directory to export
         """
 
-        pass
+        joblib.dump(self._pipeline, f"{pipeline_dir}/pipeline.joblib")
 
 
-class AbstractPipelineWithModel(AbstractPipeline):
+class PipelineGenericWithModel(PipelineGeneric):
     """
-    Abstract class for pipeline with model
+    PipelineGenericWithModel class
 
     Methods
     -------
-    _inject_model_in_pipeline() -> None
-        Abstract method to inject model in pipeline
-    _generate_pipeline_metrics() -> None
-        Abstract method to generate pipeline metrics
-    _export_pipeline_metrics() -> None
-        Abstract method to export pipeline metrics
+    _inject_model_in_pipeline(model: str) -> None
+        Inject model in pipeline
+    _generate_pipeline_metrics(model_type: str) -> None
+        Generate pipeline metrics
+    _execute_pipeline() -> None
+        Execute the pipeline
+    _export_pipeline(pipeline_dir: str) -> None
+        Export pipeline
+    _export_pipeline_metrics(pipeline_dir: str) -> None
+        Export pipeline metrics
     """
 
-    @abstractmethod
-    def _inject_model_in_pipeline(self) -> None:
+    def _inject_model_in_pipeline(self, model: str) -> None:
         """
-        Abstract method to inject model in pipeline
+        Inject model in pipeline
+
+        Parameters
+        ----------
+        model : str
+            Model to inject in the pipeline
         """
 
-        pass
+        AVAILABLE_MODELS = {
+            "logistic_regression": LogisticRegression(),
+        }
 
-    @abstractmethod
-    def _generate_pipeline_metrics(self) -> None:
+        try:
+            self._pipeline_with_model = Pipeline(
+                [("pipeline", self._pipeline), ("model", AVAILABLE_MODELS[model])]
+            )
+        except KeyError:
+            raise KeyError(f"Model {model} not available")
+
+    def _generate_pipeline_metrics(self, model_type: str) -> None:
         """
-        Abstract method to generate pipeline metrics
+        Generate pipeline metrics
+
+        Parameters
+        ----------
+        model_type : str
+            Model type to generate metrics
+
+        Raises
+        ------
+        AttributeError
+            If X or y is not defined
         """
 
-        pass
+        try:
+            self.X, self.y
+        except AttributeError as e:
+            raise AttributeError(f"{e.name} is not defined")
 
-    @abstractmethod
-    def _export_pipeline_metrics(self) -> None:
+        if model_type == "classification":
+            self._metrics = {
+                "accuracy": cross_val_score(
+                    self._pipeline_with_model, self.X, self.y, cv=5, scoring="accuracy"
+                ).mean(),
+                "precision": cross_val_score(
+                    self._pipeline_with_model, self.X, self.y, cv=5, scoring="precision"
+                ).mean(),
+                "recall": cross_val_score(
+                    self._pipeline_with_model, self.X, self.y, cv=5, scoring="recall"
+                ).mean(),
+                "f1": cross_val_score(
+                    self._pipeline_with_model, self.X, self.y, cv=5, scoring="f1"
+                ).mean(),
+            }
+
+    def _execute_pipeline(self) -> None:
         """
-        Abstract method to export pipeline metrics
+        Execute the pipeline
         """
 
-        pass
+        self._pipeline_with_model.fit(self.X, self.y)
+
+    def _export_pipeline(self, pipeline_dir: str) -> None:
+        """
+        Export pipeline
+
+        Parameters
+        ----------
+        pipeline_dir : str
+            Pipeline directory to export pipeline
+        """
+
+        joblib.dump(self._pipeline_with_model, f"{pipeline_dir}/pipeline.joblib")
+
+    def _export_pipeline_metrics(self, pipeline_dir: str) -> None:
+        """
+        Export pipeline metrics
+
+        Parameters
+        ----------
+        pipeline_dir : str
+            Pipeline directory to export metrics
+        """
+
+        with open(f"{pipeline_dir}/metrics.json", "w") as file:
+            json.dump(self._metrics, file, indent=4)
+            file.write("\n")
--- matches/management/commands/train_match_winner_model.py
+++ matches/management/commands/train_match_winner_model.py
@@ -1,25 +1,20 @@
-import json
 import os
 
-import joblib
 import pandas as pd
 from django.core.management.base import BaseCommand
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_val_score
-from sklearn.pipeline import Pipeline
 
 from ai.custom import generate_match_winner_column
-from ai.interfaces import AbstractPipelineWithModel
-from ai.preprocessing import NumericalScaler, split_xy
+from ai.interfaces import PipelineGenericWithModel
+from ai.preprocessing import split_xy
 from matches.constants import MatchFields
 from matches.models import Match
 
 pd.set_option("future.no_silent_downcasting", True)
 
-MATCH_WINNER_MODEL_DIR = os.getcwd() + "/ai/pipelines/match_winner"
+MATCH_WINNER_PIPELINE_DIR = os.getcwd() + "/ai/pipelines/match_winner"
 
 
-class Command(BaseCommand, AbstractPipelineWithModel):
+class Command(BaseCommand, PipelineGenericWithModel):
     help = "Train match winner model"
 
     def handle(self, *args, **options) -> None:
@@ -28,7 +23,7 @@ class Command(BaseCommand, AbstractPipelineWithModel):
         """
 
         self.stdout.write(self.style.HTTP_INFO("Creating pipeline directory..."))
-        os.makedirs(MATCH_WINNER_MODEL_DIR, exist_ok=True)
+        os.makedirs(MATCH_WINNER_PIPELINE_DIR, exist_ok=True)
 
         self.stdout.write(self.style.HTTP_INFO("Downloading match data..."))
         self._get_match_df()
@@ -43,25 +38,25 @@ class Command(BaseCommand, AbstractPipelineWithModel):
         self._initialize_pipeline_steps()
 
         self.stdout.write(self.style.HTTP_INFO("Adding steps to pipeline..."))
-        self._add_steps_to_pipeline()
+        self._add_steps_to_pipeline(steps=["numerical_scaler"])
 
         self.stdout.write(self.style.HTTP_INFO("Initializing pipeline..."))
         self._initialize_pipeline()
 
         self.stdout.write(self.style.HTTP_INFO("Injecting model in pipeline..."))
-        self._inject_model_in_pipeline()
+        self._inject_model_in_pipeline(model="logistic_regression")
 
         self.stdout.write(self.style.HTTP_INFO("Generating pipeline metrics..."))
-        self._generate_pipeline_metrics()
+        self._generate_pipeline_metrics(model_type="classification")
 
         self.stdout.write(self.style.HTTP_INFO("Executing pipeline..."))
         self._execute_pipeline()
 
         self.stdout.write(self.style.HTTP_INFO("Exporting pipeline..."))
-        self._export_pipeline()
+        self._export_pipeline(pipeline_dir=MATCH_WINNER_PIPELINE_DIR)
 
         self.stdout.write(self.style.HTTP_INFO("Exporting pipeline metrics..."))
-        self._export_pipeline_metrics()
+        self._export_pipeline_metrics(pipeline_dir=MATCH_WINNER_PIPELINE_DIR)
 
     def _get_match_df(self) -> None:
         """
@@ -107,79 +102,9 @@ class Command(BaseCommand, AbstractPipelineWithModel):
 
         self.y = generate_match_winner_column(df=y)
 
-    def _initialize_pipeline_steps(self) -> None:
-        """
-        Initialize the pipeline
-        """
-
-        self._pipeline_steps = []
-
-    def _add_steps_to_pipeline(self) -> None:
-        """
-        Add steps to pipeline
-        """
-
-        numerical_scaler_transformer = NumericalScaler()
-
-        self._pipeline_steps.append(("scale_features", numerical_scaler_transformer))
-
-    def _initialize_pipeline(self) -> None:
-        """
-        Initialize the pipeline
-        """
-
-        self._pipeline = Pipeline(self._pipeline_steps)
-
-    def _inject_model_in_pipeline(self) -> None:
-        """
-        Inject model in pipeline
-        """
-
-        self._pipeline_with_model = Pipeline(
-            [("pipeline", self._pipeline), ("model", LogisticRegression())]
-        )
-
-    def _generate_pipeline_metrics(self) -> None:
-        """
-        Generate pipeline metrics
-        """
-
-        self._metrics = {
-            "accuracy": cross_val_score(
-                self._pipeline_with_model, self.X, self.y, cv=5, scoring="accuracy"
-            ).mean(),
-            "precision": cross_val_score(
-                self._pipeline_with_model, self.X, self.y, cv=5, scoring="precision"
-            ).mean(),
-            "recall": cross_val_score(
-                self._pipeline_with_model, self.X, self.y, cv=5, scoring="recall"
-            ).mean(),
-            "f1": cross_val_score(
-                self._pipeline_with_model, self.X, self.y, cv=5, scoring="f1"
-            ).mean(),
-        }
-
     def _execute_pipeline(self) -> None:
         """
         Execute the pipeline
         """
 
         self._pipeline_with_model.fit(self.X, self.y)
-
-    def _export_pipeline(self) -> None:
-        """
-        Export pipeline
-        """
-
-        joblib.dump(
-            self._pipeline_with_model, f"{MATCH_WINNER_MODEL_DIR}/pipeline.joblib"
-        )
-
-    def _export_pipeline_metrics(self) -> None:
-        """
-        Export pipeline metrics
-        """
-
-        with open(f"{MATCH_WINNER_MODEL_DIR}/metrics.json", "w") as file:
-            json.dump(self._metrics, file, indent=4)
-            file.write("\n")
