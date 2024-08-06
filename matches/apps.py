99ebd48430b01ac0dd4c5014cd46f5b92650a66a
Sergio <sergio.pm.developer@gmail.com>
2024-08-06 21:12:09 +0000

    ops: initialize matches app

---
+++ matches/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class MatchesConfig(AppConfig):
+    """
+    Matches app configuration
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "matches"
