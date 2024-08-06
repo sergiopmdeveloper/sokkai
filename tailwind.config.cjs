1cad2853f83e75d0538db5344b50145724092994
Sergio <sergio.pm.developer@gmail.com>
2024-08-06 19:34:47 +0000

    style: implement tailwind theme and adapt templates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./templates/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-color1': '#001524',
+        'theme-color2': '#15616d',
+        'theme-color3': '#ffecd1',
+        'theme-color4': '#ff7d00',
+      },
+      screens: {
+        xs: '480px',
+      },
+    },
   },
   plugins: [],
 };
