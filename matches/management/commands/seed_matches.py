4134e69a6a5769107b0174db6ee200352aa9c6f6
Sergio <sergio.pm.developer@gmail.com>
2024-08-07 17:38:57 +0000

    feat: format columns and generate match instances in seed_matches command

--- matches/management/commands/seed_matches.py
+++ matches/management/commands/seed_matches.py
@@ -2,6 +2,7 @@ import pandas as pd
 from django.core.management.base import BaseCommand, CommandError
 
 from matches.constants import MatchFields
+from matches.models import Match
 
 SOCCER_MATCHES_URL = (
     "https://projects.fivethirtyeight.com/soccer-api/club/spi_matches.csv"
@@ -12,6 +13,7 @@ class Command(BaseCommand):
     help = "Command for seeding the table `matches` in the database"
 
     _match_df: pd.DataFrame
+    _match_instances: list[Match]
 
     def handle(self, *args, **options) -> None:
         """
@@ -23,6 +25,12 @@ class Command(BaseCommand):
 
         self.stdout.write("Extracting columns...")
         self._extract_columns()
+
+        self.stdout.write("Formatting columns...")
+        self._format_columns()
+
+        self.stdout.write("Generating match instances...")
+        self._generate_match_instances()
 
     def _download_match_data(self) -> None:
         """
@@ -45,3 +53,21 @@ class Command(BaseCommand):
         """
 
         self._match_df = self._match_df[MatchFields.field_list()]
+
+    def _format_columns(self) -> None:
+        """
+        Formats the columns of the match DataFrame
+        """
+
+        self._match_df[MatchFields.SEASON] = self._match_df[MatchFields.SEASON].astype(
+            str
+        )
+
+    def _generate_match_instances(self) -> None:
+        """
+        Generates the match instances from the match DataFrame
+        """
+
+        self._match_instances = [
+            Match(**match) for match in self._match_df.to_dict("records")
+        ]
