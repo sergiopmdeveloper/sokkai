647cb5cf22e2a3e19ed7a476aabb86d7fcfd8642
Sergio <sergio.pm.developer@gmail.com>
2024-08-07 23:36:58 +0000

    feat: add pagination to match results page

--- matches/views.py
+++ matches/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
+
+from matches.models import Match
 
 
 @login_required(login_url="sign-in")
@@ -19,7 +22,14 @@ def match_results(request: HttpRequest) -> HttpResponse:
         The rendered match results page
     """
 
-    return render(request, "matches/results.html")
+    match_results = Match.objects.filter(score1__isnull=False, score2__isnull=False)
+
+    paginator = Paginator(match_results, 20)
+    page = request.GET.get("page", 1)
+
+    match_results = paginator.get_page(page)
+
+    return render(request, "matches/results.html", {"matches": match_results})
 
 
 def matches(_: HttpRequest) -> HttpResponseRedirect:
